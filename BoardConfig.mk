6dd8cc3baf637b9e6b0fe577c6fa357cd7e028d7
raymanfx <raymanfx@gmail.com>
2021-04-01 22:15:33 +0200

    lmi: Enforce common and vendor trees presence

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit from xiaomi sm8250-common
--include device/xiaomi/sm8250-common/BoardConfigCommon.mk
+include device/xiaomi/sm8250-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/lmi
 
@@ -32,4 +32,4 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_lmi
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 
 # inherit from the proprietary version
--include vendor/xiaomi/lmi/BoardConfigVendor.mk
+include vendor/xiaomi/lmi/BoardConfigVendor.mk
