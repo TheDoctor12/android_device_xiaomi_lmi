7fa91c07c07164961432060beb1449aedb9fea9d
Andrzej Perczak <linux@andrzejperczak.com>
2021-12-02 15:55:13 +0100

    lmi: Don't report memcg functionality in cmdline
    Memcg was disabled in kernel as on Google devices, it is said
    to cause performance regression.
    
    Change-Id: I3f1670394666eb6527c045e7e22acaea6d8c71e9
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,7 +86,6 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_lmi
 BOARD_KERNEL_CMDLINE := \
     androidboot.hardware=qcom \
     androidboot.init_fatal_reboot_target=recovery \
-    androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3 \
     cgroup.memory=nokmem,nosocket \
     earlycon=msm_geni_serial,0xa90000 \
