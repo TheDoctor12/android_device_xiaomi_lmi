1cedf8ba649342990a0b3e700b7f81855b05c1ae
Mesquita <mesquita@aospa.co>
2023-04-21 00:52:21 -0300

    lmi: enable keymaster component
    Change-Id: Ibfb733e417035f0362035aac8ee17ec1b5b19d2c
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- device.mk
+++ device.mk
@@ -264,6 +264,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     dsprpcd \
     gps \
     init \
+    keymaster \
     media \
     nfc \
     overlay \
@@ -344,9 +345,7 @@ PRODUCT_EXTRA_VNDK_VERSIONS := 30
 
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
-    android.hardware.gatekeeper@1.0.vendor \
-    android.hardware.keymaster@4.1.vendor \
-    android.hardware.neuralnetworks@1.3.vendor \
+    android.hardware.neuralnetworks@1.3.vendor
 
 # WLAN
 PRODUCT_COPY_FILES += \
