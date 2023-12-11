3eb2048f093ab74a268cddf2a172edf13a9e4b1d
TheDoctor12 <jkaskabas95@gmail.com>
2023-12-11 03:01:49 +0200

    lmi: vintf: Add missing FCM HALs
    The following HALs in device manifest are not declared in FCM <= level 5:
      android.hardware.biometrics.fingerprint@2.3::IBiometricsFingerprint/default
      android.hardware.bluetooth.audio@2.1::IBluetoothAudioProvidersFactory/default
      android.hardware.cas.IMediaCasService/default (@1)
      android.hardware.health.IHealth/default (@1)
      android.hardware.light.ILights/default (@2)
      android.hardware.power.IPower/default (@4)
      android.hardware.vibrator.IVibrator/default (@2)
      android.hardware.wifi.IWifi/default (@1)
      android.hardware.wifi.hostapd.IHostapd/default (@1)
      android.hardware.wifi.supplicant.ISupplicant/default (@2)
    COMPATIBLE

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,7 +72,9 @@ TARGET_USES_FOD_ZPOS := true
 TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/xiaomi:libudfps_extension.xiaomi
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/vintf/device_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/configs/vintf/manifest.xml \
