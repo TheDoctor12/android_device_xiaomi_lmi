b5b5e3f4978c6b07fe51f26b5276b752de947e91
Michael Bestas <mkbestas@lineageos.org>
2024-08-25 18:52:18 +0300

    lmi: Shim widevine with libcrypto_shim
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29

--- device.mk
+++ device.mk
@@ -159,7 +159,8 @@ PRODUCT_VENDOR_PROPERTIES += \
     drm.service.enabled=true
 
 PRODUCT_PACKAGES += \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm-service.clearkey \
+    libcrypto_shim
 
 # Fastboot
 PRODUCT_PACKAGES += \
