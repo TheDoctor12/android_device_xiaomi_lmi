b71274a7914e860196eb8428a1d21149f11fe824
Bin <9633157+web1n@users.noreply.github.com>
2025-06-16 23:40:24 +0800

    lmi: Patch audio to load libaudioroute-v34.so
    Change-Id: I275c1e28871b9f017cc65bb5890e5dc0c7cbd25f

--- extract-files.py
+++ extract-files.py
@@ -51,6 +51,8 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_unlock'),
     'vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so': blob_fixup()
         .binary_regex_replace(b'\x9A\x0A\x00\x94', b'\x1F\x20\x03\xD5'),
+    ('vendor/lib/hw/audio.primary.lmi.so','vendor/lib/libaudioroute_ext.so'): blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
 }  # fmt: skip
 
 namespace_imports = [
