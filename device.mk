b8a8ab18f25d7dcb41ab15791e0be64d4f321045
Tkiliay <sackclouths@gmail.com>
2025-12-03 11:24:52 +0800

    lmi: PixelOS bringup
    Change-Id: I279634e424172b0420650b5a6ff0c5360deb963d
    Signed-off-by: TheDoctor12 <jkaskabas95@gmail.com>

--- device.mk
+++ device.mk
@@ -42,8 +42,7 @@ $(call soong_config_set,xiaomi_kona,variant_lib,//$(LOCAL_PATH):libvariant_xiaom
 PRODUCT_PACKAGES += \
     ApertureOverlayDevice \
     FrameworkResOverlayDevice \
-    LineageSDKOverlayDevice \
-    LineageSystemUIOverlayDevice \
+    CustomSystemUIOverlayDevice \
     SystemUIOverlayDevice
 
 # Permissions
